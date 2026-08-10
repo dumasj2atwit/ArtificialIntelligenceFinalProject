364e48900756f2a9123acd4dbf8b98ee8e625219
RabihElmassih <rabihelmassih16@gmail.com>
2026-08-10 19:43:43 -0400

    v0 of Q learning

---
+++ training/train_q_learning.py
@@ -0,0 +1,47 @@
+from agents.q_learning_agent import QLearningAgent
+from environment.environment import MazeEnvironment
+
+def train_q_learning(maze_file: str, episodes: int = 1000):
+
+    enviorment= MazeEnvironment(maze_file)
+
+    agent = QLearningAgent(enviorment=enviorment, learning_rate=0.2,discount_factor=0.9,exploration_rate=0.1)
+
+    rewards = []
+    steps = []
+    successes  = []
+
+    for episode in range(episodes):
+
+        enviorment.reset()
+
+        state = agent.get_state()
+
+        total_reward = 0
+        episode_steps = 0
+        done = False
+
+        while not done:
+
+            action = agent.get_action(state)
+
+            next_pos, reward, done = (
+                enviorment.step(action)
+            )
+
+            next_state = agent.get_state()
+
+            agent.update( state, action, next_state, reward, done,)
+
+            state = next_state
+
+            total_reward += reward
+            episode_steps += 1
+
+        rewards.append(total_reward)
+        steps.append(episode_steps)
+        successes.append(done)
+
+    return agent, rewards, steps, successes 
+
+
